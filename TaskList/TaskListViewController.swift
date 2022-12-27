20a643bad274710c646ce49b0af3bfc6c18024b3
BorisSonin <bsonin85@gmail.com>
2022-12-27 22:34:17 +0300

    Added func for row deleting, added alert controller for editing task

--- TaskList/TaskListViewController.swift
+++ TaskList/TaskListViewController.swift
@@ -8,21 +8,21 @@
 import UIKit
 import CoreData
 
-protocol TaskViewControllerDelegate {
-    func reloadData()
-}
+
 
 class TaskListViewController: UITableViewController {
     
     private let cellID = "task"
     private var taskList: [Task] = []
     private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
+    private var changeTextField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupNavigationBar()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        fetchData()
     }
 
     private func setupNavigationBar() {
@@ -49,9 +49,7 @@ class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        let taskVC = TaskViewController()
-        taskVC.delegate = self
-        present(taskVC, animated: true)
+        showAlert(withTitle: "New Task", andMessage: "Please add new task!")
     }
     
     private func fetchData() {
@@ -62,6 +60,45 @@ class TaskListViewController: UITableViewController {
         } catch let error {
             print("No fetch data", error)
         }
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        
+        let task = Task(context: viewContext)
+       
+        task.title = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch let error {
+                print(error)
+            }
+        }
+    
     }
 }
 
@@ -80,12 +117,36 @@ extension TaskListViewController {
         cell.contentConfiguration = content
         return cell
     }
-}
-
-// MARK: - TaskViewControllerDelegate
-extension TaskListViewController: TaskViewControllerDelegate {
-    func reloadData() {
-        fetchData()
-        tableView.reloadData()
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            taskList.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .bottom)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rowSelected = taskList[indexPath.row]
+        let alert = UIAlertController(title: "Edit Task", message: "", preferredStyle: .alert)
+        let update = UIAlertAction(title: "Update", style: .default) { [weak self] action in
+            let updateText = self?.changeTextField?.text
+//            self?.taskList[indexPath.row] = updateText
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+                print("Data has been updated in tableView")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            print("Edit operation cancelled")
+        }
+        alert.addAction(update)
+        alert.addAction(cancel)
+        alert.addTextField { (textField) in
+            self.changeTextField = textField
+//            self.changeTextField?.text = rowSelected
+            self.changeTextField?.placeholder = "Update Task"
+        }
+        present(alert, animated: true, completion: nil)
     }
 }
+
+
